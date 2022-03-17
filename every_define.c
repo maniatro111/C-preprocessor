888829150200eb1b0bdd226ddba2242934c94386
maniatro111 <florin.postolache80@gmail.com>
2022-03-17 11:28:50 +0200

    Code refactoring

--- every_define.c
+++ every_define.c
@@ -12,7 +12,8 @@ int check_not_in_between(int pos, int nr_el, int *vec)
 		return 1;
 	int i;
 
-	for (i = 0; i < nr_el; i += 2) {
+	for (i = 0; i < nr_el; i += 2)
+	{
 		if (pos > vec[i] && pos < vec[i + 1])
 			return 0;
 	}
@@ -22,19 +23,17 @@ int check_not_in_between(int pos, int nr_el, int *vec)
 void get_apostrophes(char *string, int **vect, int *el_number)
 {
 	*el_number = 0;
-	if (strchr(string, '\"')) {
+	if (strchr(string, '\"'))
+	{
 		int initial_size;
 
 		initial_size = 10;
 		*vect = (int *)malloc(initial_size * sizeof(int));
 		(*vect)[*el_number] = strchr(string, '\"') - string;
 		(*el_number)++;
-		while (1 + (*vect)[(*el_number) - 1] < strlen(string) &&
-		       strchr(string + 1 + (*vect)[(*el_number) - 1], '\"')) {
-			(*vect)[*el_number] =
-			    strchr(string + 1 + (*vect)[(*el_number) - 1],
-				   '\"') -
-			    string;
+		while (1 + (*vect)[(*el_number) - 1] < strlen(string) && strchr(string + 1 + (*vect)[(*el_number) - 1], '\"'))
+		{
+			(*vect)[*el_number] = strchr(string + 1 + (*vect)[(*el_number) - 1], '\"') - string;
 			(*el_number)++;
 		}
 	}
@@ -45,21 +44,23 @@ static void check_define_in_define(ht *tabel, char *value)
 	int j;
 
 	for (j = 0; j < tabel->capacity; j++)
-		if (tabel->entries[j].key != NULL) {
-			if (strstr(value, tabel->entries[j].key) != NULL) {
+		if (tabel->entries[j].key != NULL)
+		{
+			if (strstr(value, tabel->entries[j].key) != NULL)
+			{
 				char *aux;
 				int i;
 
 				i = strstr(value, tabel->entries[j].key) -
-				    value;
+					value;
 				aux = (char *)malloc((strlen(value) + 1) *
-						     sizeof(char));
+									 sizeof(char));
 				strcpy(aux, value);
 				strncpy(value, aux, i);
 				stpcpy(value + i, tabel->entries[j].value);
 				strcpy(value + i +
-					   strlen(tabel->entries[j].value),
-				       aux + i + strlen(tabel->entries[j].key));
+						   strlen(tabel->entries[j].value),
+					   aux + i + strlen(tabel->entries[j].key));
 				free(aux);
 				check_define_in_define(tabel, value);
 			}
@@ -78,54 +79,63 @@ void insert_define_from_file(ht *tabel, char *buf, FILE *infd)
 	key = (char *)malloc((strlen(aux) + 1) * sizeof(char));
 	strcpy(key, aux);
 	aux = strtok(NULL, "\n");
-	if (aux[strlen(aux) - 1] == '\\') {
+	if (aux[strlen(aux) - 1] == '\\')
+	{
 		char *argumente_concatenate;
 		int i;
 
 		argumente_concatenate = (char *)calloc(250, sizeof(char));
 		strcat(argumente_concatenate, aux);
 		argumente_concatenate[strlen(argumente_concatenate) - 1] = '\0';
-		for (i = strlen(argumente_concatenate) - 1;
-		     i >= 0 && (argumente_concatenate[i] == ' ' ||
-				argumente_concatenate[i] == '\t');
-		     i--)
+		for (i = strlen(argumente_concatenate) - 1; i >= 0 && (argumente_concatenate[i] == ' ' || argumente_concatenate[i] == '\t'); i--)
 			argumente_concatenate[i] = '\0';
 		fgets(buf, 256, infd);
 		buf[strlen(buf) - 1] = '\0';
-		while (buf[strlen(buf) - 1] == '\\') {
+		while (buf[strlen(buf) - 1] == '\\')
+		{
 			int i;
 
-			for (i = 0; i < strlen(buf) &&
-				    (buf[i] == ' ' || buf[i] == '\t');
-			     i++)
+			for (i = 0; i < strlen(buf) && (buf[i] == ' ' || buf[i] == '\t'); i++)
 				;
 			strcat(argumente_concatenate, " ");
 			strcat(argumente_concatenate, buf + i);
-			argumente_concatenate[strlen(argumente_concatenate) -
-					      1] = '\0';
+			argumente_concatenate[strlen(argumente_concatenate) - 1] = '\0';
 
-			for (i = strlen(argumente_concatenate);
-			     i >= 1 && (argumente_concatenate[i] == ' ' &&
-					argumente_concatenate[i - 1] == ' ');
-			     i--)
+			for (i = strlen(argumente_concatenate); i >= 1 && (argumente_concatenate[i] == ' ' && argumente_concatenate[i - 1] == ' '); i--)
 				argumente_concatenate[i] = '\0';
 			fgets(buf, 256, infd);
 			buf[strlen(buf) - 1] = '\0';
 		}
-		for (i = 0;
-		     i < strlen(buf) && (buf[i] == ' ' || buf[i] == '\t'); i++)
+		for (i = 0; i < strlen(buf) && (buf[i] == ' ' || buf[i] == '\t'); i++)
 			;
 		strcat(argumente_concatenate, " ");
 		strcat(argumente_concatenate, buf + i);
 		check_define_in_define(tabel, aux);
 		ht_set(tabel, key, argumente_concatenate);
 		free(argumente_concatenate);
-	} else {
+	}
+	else
+	{
 		check_define_in_define(tabel, aux);
 		ht_set(tabel, key, aux);
 	}
 	free(key);
 	free(argumente);
+}
+
+static void check_and_copy(int pos, int dimension, int *v, char *buf, ht *map, int map_index)
+{
+	if (check_not_in_between(pos, dimension, v))
+	{
+		char *aux;
+
+		aux = (char *)malloc(256 * sizeof(char));
+		strcpy(aux, buf);
+		strncpy(buf, aux, pos);
+		strcpy(buf + pos, map->entries[map_index].value);
+		strcpy(buf + pos + strlen(map->entries[map_index].value), aux + pos + strlen(map->entries[map_index].key));
+		free(aux);
+	}
 }
 
 void analyze_and_print(ht *map, char *buf, FILE *outfd)
@@ -137,59 +147,44 @@ void analyze_and_print(ht *map, char *buf, FILE *outfd)
 	int j;
 
 	for (j = 0; j < (int)map->capacity; j++)
-		if (map->entries[j].key != NULL) {
-			if (strstr(buf, map->entries[j].key)) {
-				int pos =
-				    strstr(buf, map->entries[j].key) - buf;
-				if (check_not_in_between(pos, n, v)) {
-					char *aux;
-
-					aux =
-					    (char *)malloc(256 * sizeof(char));
-					strcpy(aux, buf);
-					strncpy(buf, aux, pos);
-					strcpy(buf + pos,
-					       map->entries[j].value);
-					strcpy(
-					    buf + pos +
-						strlen(map->entries[j].value),
-					    aux + pos +
-						strlen(map->entries[j].key));
-					free(aux);
-				}
-				while (strstr(buf + pos +
-						  strlen(map->entries[j].key),
-					      map->entries[j].key)) {
-					pos =
-					    strstr(
-						buf + pos +
-						    strlen(map->entries[j].key),
-						map->entries[j].key) -
-					    buf;
-					if (check_not_in_between(pos, n, v)) {
-
-						char *aux;
-
-						aux = (char *)malloc(
-						    256 * sizeof(char));
-						strcpy(aux, buf);
-						strncpy(buf, aux, pos);
-						strcpy(buf + pos,
-						       map->entries[j].value);
-						strcpy(
-						    buf + pos +
-							strlen(map->entries[j]
-								   .value),
-						    aux + pos +
-							strlen(map->entries[j]
-								   .key));
-						free(aux);
-					}
-				}
+		if (map->entries[j].key != NULL && strstr(buf, map->entries[j].key))
+		{
+			int pos = strstr(buf, map->entries[j].key) - buf;
+			check_and_copy(pos, n, v, buf, map, j);
+			while (strstr(buf + pos + strlen(map->entries[j].key), map->entries[j].key))
+			{
+				pos = strstr(buf + pos + strlen(map->entries[j].key), map->entries[j].key) - buf;
+				check_and_copy(pos, n, v, buf, map, j);
 			}
 		}
 
 	fprintf(outfd, "%s", buf);
 	if (n)
 		free(v);
+}
+
+void add_argument_mapping(char **argv, int *line, ht *map)
+{
+	if (strlen(argv[*line]) > 2)
+		argv[*line] = argv[*line] + 2;
+	else
+		(*line)++;
+
+	if (strchr(argv[*line], '=') != NULL)
+	{
+		char *aux;
+
+		aux = strtok(argv[*line], "=");
+		ht_set(map, argv[*line], argv[*line] + strlen(argv[*line]) + 1);
+	}
+	else
+	{
+		ht_set(map, argv[*line], "");
+	}
+}
+
+int undefine_key(ht *map, char *key)
+{
+	key[strlen(key) - 1] = '\0';
+	delete_entry(map, key + 7);
 }
--- main.c
+++ main.c
@@ -23,76 +23,70 @@ int main(int argc, char **argv)
 	outfile = NULL;
 
 	map = ht_create();
-	for (i = 1; i < argc; i++) {
-		if (argv[i][0] == '-') {
-			if (argv[i][1] == 'D') {
-				if (strlen(argv[i]) > 2)
-					argv[i] = argv[i] + 2;
-				else
-					i++;
-
-				if (strchr(argv[i], '=') != NULL) {
-					char *aux;
-
-					aux = strtok(argv[i], "=");
-					ht_set(map, argv[i],
-					       argv[i] + strlen(argv[i]) + 1);
-				} else {
-					ht_set(map, argv[i], "");
-				}
-			} else if (argv[i][1] == 'I') {
-			} else if (argv[i][1] == 'o') {
-			} else {
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i][0] == '-')
+		{
+			if (argv[i][1] == 'D')
+				add_argument_mapping(argv, &i, map);
+			else if (argv[i][1] == 'I')
+			{
+			}
+			else if (argv[i][1] == 'o')
+			{
+			}
+			else
 				return 1;
-			}
-		} else {
-			if (infile == NULL) {
-				infile = (char *)malloc((strlen(argv[i]) + 1) *
-							sizeof(char));
-				strcpy(infile, argv[i]);
-			} else {
-				if (outfile == NULL) {
-					outfile = (char *)malloc(
-					    (strlen(argv[i]) + 1) *
-					    sizeof(char));
-					strcpy(outfile, argv[i]);
-				} else {
-					return 1;
-				}
-			}
 		}
+		else if (infile == NULL)
+		{
+			infile = (char *)malloc((strlen(argv[i]) + 1) * sizeof(char));
+			strcpy(infile, argv[i]);
+		}
+		else if (outfile == NULL)
+		{
+			outfile = (char *)malloc((strlen(argv[i]) + 1) * sizeof(char));
+			strcpy(outfile, argv[i]);
+		}
+		else
+			return 1;
 	}
 
-	if (infile != NULL) {
+	if (infile != NULL)
+	{
 		infd = fopen(infile, "r");
 		if (infd == NULL)
 			return 1;
 	}
-	if (outfile != NULL) {
+	if (outfile != NULL)
+	{
 		outfd = fopen(outfile, "w+");
 		if (outfd == NULL)
 			return 1;
 	}
 
-	while (fgets(buf, 256, infd)) {
-		if (buf[0] == '#') {
-			if (strncmp(buf + 1, "define", 6) == 0) {
+	while (fgets(buf, 256, infd))
+	{
+		if (buf[0] == '#')
+		{
+			if (strncmp(buf + 1, "define", 6) == 0)
 				insert_define_from_file(map, buf + 8, infd);
-			} else if (strncmp(buf + 1, "undef", 5) == 0) {
-				buf[strlen(buf) - 1] = '\0';
-				delete_entry(map, buf + 7);
-			}
-		} else if (strlen(buf) == 1 && buf[0] == '\n')
+			else if (strncmp(buf + 1, "undef", 5) == 0)
+				undefine_key(map, buf);
+		}
+		else if (strlen(buf) == 1 && buf[0] == '\n')
 			;
 		else
 			analyze_and_print(map, buf, outfd);
 	}
 
-	if (infile != NULL) {
+	if (infile != NULL)
+	{
 		free(infile);
 		fclose(infd);
 	}
-	if (outfile != NULL) {
+	if (outfile != NULL)
+	{
 		free(outfile);
 		fclose(outfd);
 	}

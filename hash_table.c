c52fbdb0ae6592df468cc21ef2200b6ab116ce30
maniatro111 <florin.postolache80@gmail.com>
2022-03-19 20:44:21 +0200

    Ready for Windows checking

--- hash_table.c
+++ hash_table.c
@@ -1,4 +1,3 @@
-// Simple hash table implemented in C.
 #include <stdlib.h>
 #include <string.h>
 #ifndef HASH_TABLE
@@ -6,24 +5,19 @@
 #include "hash_table.h"
 #endif
 
-// Hash table entry (slot may be filled or empty).
-
-#define INITIAL_CAPACITY 2 // must not be zero
+#define INITIAL_CAPACITY 2
 
 int map_create(map **table)
 {
-	// Allocate space for hash table struct.
 	(*table) = (map *)malloc(sizeof(map));
 	if ((*table) == NULL)
 		return 12;
 	(*table)->length = 0;
 	(*table)->capacity = INITIAL_CAPACITY;
-
-	// Allocate (zero'd) space for entry buckets.
 	(*table)->entries =
 	    (map_entry *)calloc((*table)->capacity, sizeof(map_entry));
 	if ((*table)->entries == NULL) {
-		free((*table)); // error, free table before we return!
+		free((*table));
 		return 12;
 	}
 	return 0;
@@ -31,7 +25,6 @@ int map_create(map **table)
 
 void map_destroy(map *table)
 {
-	// First free allocated keys.
 	unsigned long i;
 
 	for (i = 0; i < table->capacity; i++)
@@ -39,32 +32,24 @@ void map_destroy(map *table)
 			free(table->entries[i].key);
 			free(table->entries[i].value);
 		}
-
-	// Then free entries array and table itself.
 	free(table->entries);
 	free(table);
 }
 
-#define FNV_OFFSET 2166136261UL
-#define FNV_PRIME 16777619UL
-
-static unsigned long hash_key(char *key)
+static unsigned long hash_function(unsigned char *str)
 {
-	unsigned long hash = FNV_OFFSET;
-	char *p;
+	unsigned long hash = 5381;
+	int c;
 
-	for (p = key; *p; p++) {
-		hash ^= (unsigned long)(unsigned char)(*p);
-		hash *= FNV_PRIME;
-	}
+	while (c = *str++)
+		hash = ((hash << 5) + hash) + c;
+
 	return hash;
 }
 
 int delete_entry(map *table, char *key)
 {
-	unsigned long hash = hash_key(key);
-	unsigned long index =
-	    (unsigned long)(hash & (unsigned long)(table->capacity - 1));
+	unsigned long index = hash_function(key) % table->capacity;
 
 	if (table->entries[index].key != NULL &&
 	    strcmp(table->entries[index].key, key) == 0) {
@@ -77,17 +62,11 @@ int delete_entry(map *table, char *key)
 
 char *map_get(map *table, char *key)
 {
-	// AND hash with capacity-1 to ensure it's within entries array.
-	unsigned long hash = hash_key(key);
-	unsigned long index =
-	    (unsigned long)(hash & (unsigned long)(table->capacity - 1));
+	unsigned long index = hash_function(key) % table->capacity;
 
-	// Loop till we find an empty entry.
 	while (table->entries[index].key != NULL) {
 		if (strcmp(key, table->entries[index].key) == 0)
-			// Found key, return value.
 			return table->entries[index].value;
-		// Key wasn't in this slot, move to next (linear probing).
 		index = (index + 1) % table->capacity;
 	}
 	return NULL;
@@ -95,17 +74,11 @@ char *map_get(map *table, char *key)
 
 int macro_defined(map *table, char *key)
 {
-	// AND hash with capacity-1 to ensure it's within entries array.
-	unsigned long hash = hash_key(key);
-	unsigned long index =
-	    (unsigned long)(hash & (unsigned long)(table->capacity - 1));
+	unsigned long index = hash_function(key) % table->capacity;
 
-	// Loop till we find an empty entry.
 	while (table->entries[index].key != NULL) {
 		if (strcmp(key, table->entries[index].key) == 0)
-			// Found key, return value.
 			return 1;
-		// Key wasn't in this slot, move to next (linear probing).
 		index = (index + 1) % table->capacity;
 	}
 	return 0;
@@ -114,20 +87,13 @@ int macro_defined(map *table, char *key)
 static int map_set_entry(map_entry *entries, unsigned long capacity, char *key,
 			 char *value, unsigned long *plength)
 {
-	// AND hash with capacity-1 to ensure it's within entries array.
-	unsigned long hash = hash_key(key);
-	unsigned long index =
-	    (unsigned long)(hash & (unsigned long)(capacity - 1));
+	unsigned long index = hash_function(key) % capacity;
 
-	// Loop till we find an empty entry.
 	while (entries[index].key != NULL) {
 		if (strcmp(key, entries[index].key) == 0)
 			return 0;
-		// Key wasn't in this slot, move to next (linear probing).
 		index = (index + 1) % capacity;
 	}
-
-	// Didn't find key, allocate+copy if needed, then insert it.
 	if (key == NULL)
 		return -1;
 	entries[index].key = (char *)malloc((strlen(key) + 1) * sizeof(char));
@@ -143,24 +109,20 @@ static int map_set_entry(map_entry *entries, unsigned long capacity, char *key,
 	return 0;
 }
 
-// Expand hash table to twice its current size. Return true on success,
-// false if out of memory.
 static int map_expand(map *table)
 {
-	// Allocate new entries array.
 	unsigned long i;
 	map_entry *new_entries;
 	unsigned long new_capacity = table->capacity * 2;
 	int return_value = 1;
 
 	if (new_capacity < table->capacity)
-		return 0; // overflow (capacity would be too big)
+		return 0;
 	new_entries = calloc(new_capacity, sizeof(map_entry));
 	if (new_entries == NULL)
 		return 0;
 
 	table->length = 0;
-	// Iterate entries, move all non-empty ones to new table's entries.
 	for (i = 0; i < table->capacity && return_value == 1; i++) {
 		map_entry entry = table->entries[i];
 		if (entry.key != NULL) {
@@ -175,8 +137,6 @@ static int map_expand(map *table)
 			free(table->entries[i].value);
 		}
 	}
-
-	// Free old entries array and update this table's details.
 	free(table->entries);
 	table->entries = new_entries;
 	table->capacity = new_capacity;
@@ -187,12 +147,9 @@ int map_set(map *table, char *key, char *value)
 {
 	if (value == NULL)
 		return 1;
-
-	// If length will exceed half of current capacity, expand it.
 	if (table->length >= table->capacity / 2)
 		if (!map_expand(table))
 			return 12;
-	//  Set entry and update length.
 	return map_set_entry(table->entries, table->capacity, key, value,
 			     &table->length);
 }
--- hash_table.h
+++ hash_table.h
@@ -1,30 +1,20 @@
-// Hash table structure: create with map_create, free with map_destroy.
 typedef struct map_entry {
-	char *key; // key is NULL if this slot is empty
+	char *key;
 	char *value;
 } map_entry;
 
-// Hash table structure: create with map_create, free with map_destroy.
 typedef struct map {
-	map_entry *entries;	// hash slots
-	unsigned long capacity; // size of _entries array
-	unsigned long length;	// number of items in hash table
+	map_entry *entries;
+	unsigned long capacity;
+	unsigned long length;
 } map;
 
-// Create hash table and return pointer to it, or NULL if out of memory.
 int map_create(map **table);
 
-// Free memory allocated for hash table, including allocated keys.
 void map_destroy(map *table);
 
-// Get item with given key (NUL-terminated) from hash table. Return
-// value (which was set with map_set), or NULL if key not found.
 char *map_get(map *table, char *key);
 
-// Set item with given key (NUL-terminated) to value (which must not
-// be NULL). If not already present in table, key is copied to newly
-// allocated memory (keys are freed automatically when map_destroy is
-// called). Return address of copied key, or NULL if out of memory.
 int map_set(map *table, char *key, char *value);
 
 int delete_entry(map *table, char *key);
